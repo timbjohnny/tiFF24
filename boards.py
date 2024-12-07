e4ce471827c6b3d9edc1901ccaf62430cec8e53c
fabianthanei <Fabian.thanei@web.de>
2024-12-07 17:03:11 +0100

    lvl 2 added on board

--- boards.py
+++ boards.py
@@ -5,7 +5,7 @@ class Board:
     def __init__(self):
         pygame.mixer.pre_init(22050, -16, 1, 64)  # Lower frequency, mono, tiny buffer
         pygame.mixer.init(22050, -16, 1, 64)
-        self.boards = [
+        self.boards1 = [
         [6, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5],
         [3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
         [3, 1, 6, 4, 5, 1, 6, 4, 4, 5, 1, 3, 3, 1, 6, 4, 4, 5, 1, 6, 4, 5, 1, 3],
@@ -35,19 +35,50 @@ class Board:
         [7, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 8, 7, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 8],
         [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
         ]
+        self.boards2 = [
+        [6, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5],
+        [3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
+        [3, 1, 6, 5, 1, 6, 4, 4, 4, 4, 5, 1, 6, 5, 1, 3],
+        [3, 1, 3, 3, 1, 7, 4, 5, 6, 4, 8, 1, 3, 3, 1, 3],
+        [3, 1, 3, 3, 1, 1, 1, 3, 3, 1, 1, 1, 3, 3, 1, 3],
+        [3, 1, 3, 7, 4, 5, 1, 3, 3, 1, 6, 4, 8, 3, 1, 3],
+        [3, 1, 3, 6, 4, 8, 1, 7, 8, 1, 7, 4, 5, 3, 1, 3],
+        [3, 1, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 1, 3],
+        [3, 1, 3, 3, 1, 6, 2, 2, 2, 2, 5, 1, 3, 3, 1, 3],
+        [3, 1, 3, 3, 1, 3, 0, 0, 0, 0, 3, 1, 3, 3, 1, 3],
+        [3, 1, 3, 3, 1, 7, 4, 4, 4, 4, 8, 1, 3, 3, 1, 3],
+        [3, 1, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 1, 3],
+        [3, 1, 3, 7, 4, 5, 1, 6, 5, 1, 6, 4, 8, 3, 1, 3],
+        [3, 1, 3, 6, 4, 8, 1, 3, 3, 1, 7, 4, 5, 3, 1, 3],
+        [3, 1, 3, 3, 1, 1, 1, 3, 3, 1, 1, 1, 3, 3, 1, 3],
+        [3, 1, 3, 3, 1, 6, 4, 8, 7, 4, 5, 1, 3, 3, 1, 3],
+        [3, 1, 7, 8, 1, 7, 4, 4, 4, 4, 8, 1, 7, 8, 1, 3],
+        [3, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 3],
+        [3, 1, 6, 4, 4, 5, 1, 6, 5, 1, 6, 4, 4, 5, 1, 3],
+        [7, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 8]  
+        ]
         self.dir_path = os.path.dirname(__file__)
         self.channels = [pygame.mixer.Channel(i) for i in range(2)]
         self.current_channel = 0
         self.chomp_sound = pygame.mixer.Sound(f'{self.dir_path}/sounds/pacman_chomp.wav')
         self.chomp_sound.set_volume(0.1)
-    def get_board(self):
-        return self.boards    
+    def get_board(self, lvl):
+        if lvl == 1:
+            return self.boards1    
+        elif lvl == 2:
+            return self.boards2
     
-    def get_boardI(self, i):
-        return self.boards[i]   
+    def get_boardI(self, lvl , i):
+        if lvl == 1:
+            return self.boards1[i]   
+        elif lvl == 2:
+            return self.boards2[i]
     
-    def get_boardIJ(self, i, j):
-        return self.boards[i][j]
+    def get_boardIJ(self, lvl, i, j):
+        if lvl == 1:
+            return self.boards1[i][j]   
+        elif lvl == 2:
+            return self.boards2[i][j]
     
     def play_chomp_sound(self):
         # Round-robin through channels for immediate playback
@@ -55,27 +86,46 @@ class Board:
         channel.play(self.chomp_sound)
         self.current_channel = (self.current_channel + 1) % len(self.channels)
 
-    def set_boardXY(self, i, j, newValue):
-        if self.boards[i][j] == 1 and newValue == 0:
-            self.play_chomp_sound()
-            self.boards[i][j] = newValue   
+    def set_boardXY(self, lvl, i, j, newValue):
+        if lvl == 1:
+            if self.boards1[i][j] == 1 and newValue == 0:
+                self.play_chomp_sound()
+                self.boards1[i][j] = newValue
+                
+        elif lvl == 2:
+            if self.boards2[i][j] == 1 and newValue == 0:
+                self.play_chomp_sound()
+                self.boards2[i][j] = newValue
+           
 
-    def checkVictory(self):
+    def checkVictory(self, lvl):
         victory = False
         foodFound = False
-        for i in range(len(self.boards)):
-            for j in range(len(self.boards[i])):
-                if self.boards[i][j] == 1:
-                    foodFound = True
-                    break                              
-            if foodFound:
-                break
-        if not foodFound:
-            victory = True
+        if lvl == 1:
+            for i in range(len(self.boards1)):
+                    for j in range(len(self.boards1[i])):
+                        if self.boards1[i][j] == 1:
+                            foodFound = True
+                            break                              
+                    if foodFound:
+                        break
+                    if not foodFound:
+                        victory = True
+        elif lvl == 2:
+            for i in range(len(self.boards2)):
+                    for j in range(len(self.boards2[i])):
+                        if self.boards2[i][j] == 1:
+                            foodFound = True
+                            break                              
+                    if foodFound:
+                        break
+                    if not foodFound:
+                        victory = True
+                        
         return victory        
     
     def resetBoard(self):
-        self.boards = [
+        self.boards1 = [
         [6, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5],
         [3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
         [3, 1, 6, 4, 5, 1, 6, 4, 4, 5, 1, 3, 3, 1, 6, 4, 4, 5, 1, 6, 4, 5, 1, 3],
@@ -104,4 +154,26 @@ class Board:
         [3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
         [7, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 8, 7, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 8],
         [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+        ]
+        self.boards2 = [
+        [6, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5],
+        [3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
+        [3, 1, 6, 5, 1, 6, 4, 4, 4, 4, 5, 1, 6, 5, 1, 3],
+        [3, 1, 3, 3, 1, 7, 4, 5, 6, 4, 8, 1, 3, 3, 1, 3],
+        [3, 1, 3, 3, 1, 1, 1, 3, 3, 1, 1, 1, 3, 3, 1, 3],
+        [3, 1, 3, 7, 4, 5, 1, 3, 3, 1, 6, 4, 8, 3, 1, 3],
+        [3, 1, 3, 6, 4, 8, 1, 7, 8, 1, 7, 4, 5, 3, 1, 3],
+        [3, 1, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 1, 3],
+        [3, 1, 3, 3, 1, 6, 2, 2, 2, 2, 5, 1, 3, 3, 1, 3],
+        [3, 1, 3, 3, 1, 3, 0, 0, 0, 0, 3, 1, 3, 3, 1, 3],
+        [3, 1, 3, 3, 1, 7, 4, 4, 4, 4, 8, 1, 3, 3, 1, 3],
+        [3, 1, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 1, 3],
+        [3, 1, 3, 7, 4, 5, 1, 6, 5, 1, 6, 4, 8, 3, 1, 3],
+        [3, 1, 3, 6, 4, 8, 1, 3, 3, 1, 7, 4, 5, 3, 1, 3],
+        [3, 1, 3, 3, 1, 1, 1, 3, 3, 1, 1, 1, 3, 3, 1, 3],
+        [3, 1, 3, 3, 1, 6, 4, 8, 7, 4, 5, 1, 3, 3, 1, 3],
+        [3, 1, 7, 8, 1, 7, 4, 4, 4, 4, 8, 1, 7, 8, 1, 3],
+        [3, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 3],
+        [3, 1, 6, 4, 4, 5, 1, 6, 5, 1, 6, 4, 4, 5, 1, 3],
+        [7, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 8]  
         ]
